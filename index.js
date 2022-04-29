46d683e7a7e32e1f88bfc1128444d84e3e1868c1
Mattherw <officialmatthughes>
2022-04-29 13:35:04 -0700

    commit tree 847c4d2379c1e2d4b30d0bfb3f548596cf2ac2a6

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function divide() {
+
+}
+
+function multiply() {
+
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(number) {
+    return (number += 1);
+}
+
+function decrement(number){
+    return (number -=1);
+}
+
+function makeInt(number) {
+    return parseInt(number, 10);
+}
+
+function preserveDecimal(number) {
+    return parseFloat(number);
+}
